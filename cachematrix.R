060921ecf39d13c45c64978434946f243ea172ff
kellybin <binder.kelly@gmail.com>
2020-06-10 14:57:51 -0400

    added explanation of second function

--- cachematrix.R
+++ cachematrix.R
@@ -21,6 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+# This function either populates or retrieves the inverse from the object of type makeCacheMatrix(). It attempts to retrieve the inverse matrix from the object passed in as an argument. First, it calls the getinverse() function and checks to see if the result is null. If it's not, it returns the value with the message "Getting cached data". If it is null, it gets the matrix from the input object, calculates the inverse, uses the setInverse() function on the input object to set the inverse in the input object an then returns the value of the inverse to the parent envirnment by printing the inverse matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
